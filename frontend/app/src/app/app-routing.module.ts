d811bbf3008f8bf5462d19f6842c7972262c26a7
lewisc99 <lewiscontato99@gmail.com>
2023-06-18 16:32:09 -0300

    created static main layout

--- frontend/app/src/app/app-routing.module.ts
+++ frontend/app/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { BlogCreateComponent } from './components/blog-create/blog-create.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path:"login", component:LoginComponent},
+  {path:"blog", component:BlogComponent},
+  {path:"blog/create", component:BlogCreateComponent},
+  {path:"",component:HomeComponent},
+  {path:"**", redirectTo:"",pathMatch:"full"}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- frontend/app/src/app/app.module.ts
+++ frontend/app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { BlogCreateComponent } from './components/blog-create/blog-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    LoginComponent,
+    NavComponent,
+    HomeComponent,
+    BlogComponent,
+    BlogCreateComponent
   ],
   imports: [
     BrowserModule,
